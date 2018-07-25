6fdc3bc773ed15dd8b13928d83a46ddf3e2ad5ad
MYQ1996 <954663633@qq.com>
2018-07-25 17:36:48 +0800

    文档

--- server-node/app.js
+++ server-node/app.js
@@ -15,7 +15,7 @@ app.engine('.html', ejs.__express);
 app.set('view engine', 'jade');
 app.set('view engine', 'html');
 
-
+ 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
